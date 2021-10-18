79eeabb5ed5605ac6debb7b09ecd1a153538d7d1
giovandro <giovandrofabiosantos@hotmail.com>
2021-10-18 10:23:21 -0300

    Trabalhando Com Spring

---
+++ Spring/spring-context/src/main/java/br/com/devmedia/cursosspring/springcontext/SpringContextApplication.java
@@ -0,0 +1,13 @@
+package br.com.devmedia.cursosspring.springcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContextApplication.class, args);
+	}
+
+}
