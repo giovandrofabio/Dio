5c33a8e9cfd78469a7d9c00096a94118e4a18b06
giovandrofabio <giovandrofabiosantos@hotmail.com>
2021-10-25 16:09:31 -0300

    Estudo referente ao Selenion

---
+++ Selenion/vsmInformatica/src/main/java/com/vsminformatica/vsmInformatica/VsmInformaticaApplication.java
@@ -0,0 +1,13 @@
+package com.vsminformatica.vsmInformatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsmInformaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsmInformaticaApplication.class, args);
+	}
+
+}
---
+++ Selenion/vsmInformatica/src/test/java/com/vsminformatica/vsmInformatica/VsmInformaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsminformatica.vsmInformatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsmInformaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
